042352f05dec44aac9631e1da67ea73e62b0b132
YngvN <y.nykaas@gmail.com>
2024-05-13 19:35:21 +0200

    Pre-nav fixing

--- src/pages/home/venueList.tsx
+++ src/pages/home/venueList.tsx
@@ -1,49 +1,10 @@
-// import React, { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { fetchVenuesStart, fetchVenuesSuccess, fetchVenuesFailure } from '../../utility/redux/redux.venuesSlice';
-// import { RootState } from '../../utility/redux/redux.store';
-// import VenueComponent from './venueTile';
-// import { fetchAllVenues } from '../../utility/actions/venues/fetchVenues';
-
-// const VenueList = () => {
-//     const dispatch = useDispatch();
-//     const { venues, loading, error } = useSelector((state: RootState) => state.venues);
-
-//     useEffect(() => {
-//         dispatch(fetchVenuesStart());
-//         fetchAllVenues()
-//             .then(data => {
-//                 console.log("Fetched venues data:", data); // Log the fetched data
-//                 dispatch(fetchVenuesSuccess(data));
-//             })
-//             .catch(err => {
-//                 console.error("Error fetching venues:", err.message);
-//                 dispatch(fetchVenuesFailure(err.message));
-//             });
-//     }, [dispatch]);
-
-
-//     if (loading) return <p>Loading...</p>;
-//     if (error) return <p>Error loading venues: {error}</p>;
-//     console.log(venues);
-
-//     return (
-//         <div>
-//             {Array.isArray(venues) && venues.map(venue => <VenueComponent key={venue.id} venue={venue} />)}
-//         </div>
-//     );
-
-// };
-
-// export default VenueList;
-
-
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchVenuesStart, fetchVenuesSuccess, fetchVenuesFailure } from '../../utility/redux/redux.venuesSlice';
 import { RootState } from '../../utility/redux/redux.store';
 import VenueComponent from './venueTile';
 import { fetchAllVenues } from '../../utility/actions/venues/fetchVenues';
+import { Card, Row, Col } from 'react-bootstrap';
 
 const VenueList = () => {
     const dispatch = useDispatch();
@@ -53,7 +14,7 @@ const VenueList = () => {
         dispatch(fetchVenuesStart());
         fetchAllVenues()
             .then(data => {
-                console.log("Fetched venues data:", data);
+                // console.log("Fetched venues data:", data);
                 dispatch(fetchVenuesSuccess(data));
             })
             .catch(err => {
@@ -68,16 +29,32 @@ const VenueList = () => {
     // Check if venues is not undefined before mapping over it
     if (!venues) return null;
 
+
     return (
-        <div>
-            {venues.map(venue => (
-                <div key={venue.id}>
-                    <h2>{venue.name}</h2>
-                    {/* Render other venue details here */}
-                </div>
-            ))}
+        <div className='venue-list'>
+            <Row xs={1} md={3} className="g-4">
+                {venues.map(venue => (
+                    <Col key={venue.id}>
+                        <Card>
+                            <div className='image-gallery'>
+                                {venue.media.length > 0 && (
+                                    <img src={venue.media[0].url} alt={`Media`} className='card-img-top' />
+                                )}
+                            </div>
+                            <Card.Body>
+                                <Card.Title>{venue.name}</Card.Title>
+                                <Card.Text className='price'>Price: {venue.price}</Card.Text>
+                                <Card.Text className='rating'>Rating: {venue.rating}</Card.Text>
+                                <Card.Text className='city'>City: {venue.location.city}</Card.Text>
+                                {/* Render other venue details here */}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </div>
     );
+
 };
 
 export default VenueList;
